d67197c7135cdb0fd1aa9b3280172c633c18030b
IlliaShyn <shinilal4002@gmail.com>
2024-09-11 17:31:22 +0300

    Initial commit

---
+++ src/main/java/com/example/imagerec/ImageRecApplication.java
@@ -0,0 +1,13 @@
+package com.example.imagerec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageRecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageRecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imagerec/ImageRecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imagerec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageRecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
